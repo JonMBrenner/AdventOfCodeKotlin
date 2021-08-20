1e73d5ca7adc9ee636cddfa1c82a0eb0cecb6d92
Jon Brenner <brenn563@umn.edu>
2021-08-20 14:33:37 -0500

    add day 2 part 2 solution

--- day_2/day2.kt
+++ day_2/day2.kt
@@ -1,6 +1,6 @@
 import java.io.File
 
-fun isValidPassword(inputLine: String): Boolean {
+fun isValidPart1(inputLine: String): Boolean {
   val rule = inputLine.substringBefore(": ")
   val password = inputLine.substringAfter(": ")
 
@@ -13,8 +13,21 @@ fun isValidPassword(inputLine: String): Boolean {
   return letterCount in min..max
 }
 
+fun isValidPart2(inputLine: String): Boolean {
+  val rule = inputLine.substringBefore(": ")
+  val password = inputLine.substringAfter(": ")
+
+  val letter = rule.last()
+  val indices = rule.substringBefore(" ")
+  val idx1 = indices.substringBefore("-").toInt() - 1
+  val idx2 = indices.substringAfter("-").toInt() - 1
+
+  return (password[idx1] == letter) xor (password[idx2] == letter)
+}
+
 fun main() {
   val lines = File("input.txt").useLines { it.toList() }
-  println("Part 1: " + lines.count { isValidPassword(it) })
+  println("Part 1: " + lines.count { isValidPart1(it) })
+  println("Part 2: " + lines.count { isValidPart2(it) })
 }
 
